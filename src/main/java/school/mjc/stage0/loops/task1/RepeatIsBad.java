f00cc91bc336e95bbb9a71f7c8b9ced2518b0617
OlzhasAlseitov <olzhas010111@gmail.com>
2023-10-22 00:02:39 +0600

    Stage 0 - Module 4.2 - 1 -2

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad(int count){
         if (count > 0) {
             System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
             repeatIsBad(count - 1);
